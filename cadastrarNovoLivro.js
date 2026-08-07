490b1365dec4910f38a6da5db585de000b7bb17a
NinoskaPalmares <palmaresninoska2@gmail.com>
2026-08-07 19:21:31 -0300

    agrego sistema de cadastro de novos livros

---
+++ cadastrarNovoLivro.js
@@ -0,0 +1,23 @@
+// creado para la iteracion con el usurio
+
+import cadastrarLivro from "./cadastrarLivro.js"; 
+function cadastrarNovoLivro(teclado, iniciarSistema) {
+
+teclado.question("Digite o título: ", (titulo) => { 
+teclado.question("Digite o autor: ", (autor) => { 
+teclado.question("Digite a categoria: ", (categoria) => { 
+teclado.question("Digite o número de páginas: ", (paginas) => { 
+
+    cadastrarLivro(
+titulo, 
+autor, 
+categoria, 
+Number(paginas) 
+); 
+iniciarSistema(); 
+});
+ }); 
+}); 
+});
+ } 
+export default cadastrarNovoLivro;

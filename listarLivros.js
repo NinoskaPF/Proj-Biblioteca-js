8fba495dd81bac88b4ce34b0459cc1f4a17fd32a
NinoskaPalmares <palmaresninoska2@gmail.com>
2026-08-07 21:34:45 -0300

    feat: melhorar a estetica da funcionalidade

--- listarLivros.js
+++ listarLivros.js
@@ -5,26 +5,26 @@ import dadosDeLivros from "./dadosDeLivro.js";
 
 function listarLivros() {
 
-    console.log("lista de livros");
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");    //por estética en la consola, para que sea mas dinamico el menu de usuario
+    console.log("        LISTA DE LIVROS");                                             
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");     
 
-dadosDeLivros.forEach((livro,index) =>{
-let disponibilidade;
-if (livro.desponivel) {
-    if (livro.disponivel) {
-        disponibilidade = "Sim";
-        } else {
-disponibilidade = "Não";
-        }
-console.log(`Livro ${index+1}
-    Título: ${livro.titulo}
-    Autor: ${livro.autor}
-    Categoria:${livro.categoria}
-    Páginas: ${livro.paginas}
-    Disponível: ${livro.disponibilidade}
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    `);
-    }
-});
-}
- export default listarLivros;
+dadosDeLivros.forEach((livro, index) => { 
+let disponibilidade; 
+if (livro.disponivel) { 
+disponibilidade = "Sim"; 
+} else {
+ disponibilidade = "Não"; 
+} 
+console.log(` Livro ${index + 1} 
+Título: ${livro.titulo}
+ Autor: ${livro.autor} 
+Categoria: ${livro.categoria} 
+Páginas: ${livro.paginas} 
+Disponível: ${disponibilidade} 
+--------------------------------- 
+`);
+ });
+ } 
+export default listarLivros;
  
